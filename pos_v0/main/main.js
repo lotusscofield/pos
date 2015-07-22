929100287769002dedb0ed028cd467f09ce15ddb
sissie <sissiescofield@outlook.com>
2015-07-22 23:58:35 +0800

    7-22 homework

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,31 +1,29 @@
+var inputs = [
+    {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00,
+      count: 5
+    },
+    {
+      barcode: 'ITEM000001',
+      name: '雪碧',
+      unit: '瓶',
+      price: 3.00,
+      count: 2
+    },
+    {
+      barcode: 'ITEM000004',
+      name: '电池',
+      unit: '个',
+      price: 2.00,
+      count: 1
+    }
+  ];
+
 function printReceipt(inputs) {
-
-  inputs = [
-      {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00,
-        count: 5
-      },
-      {
-        barcode: 'ITEM000001',
-        name: '雪碧',
-        unit: '瓶',
-        price: 3.00,
-        count: 2
-      },
-      {
-        barcode: 'ITEM000004',
-        name: '电池',
-        unit: '个',
-        price: 2.00,
-        count: 1
-      }
-    ];
-
   var content = [];
-
   inputs.forEach(function(val){
     content.push('名称：' + val.name + '，数量：' + val.count + val.unit + '，单价：' + val.price.toFixed(2)  + '(元)，小计：' + (val.count*val.price).toFixed(2)  + '(元)' + '\n');
   });
@@ -36,7 +34,6 @@ function printReceipt(inputs) {
   });
 
   var receipt = '***<没钱赚商店>收据***' + '\n' + content + '----------------------' + '\n' + '总计：' + sum.toFixed(2) + '(元)' + '\n' + '**********************';
-
+//由于content是由push()生成的，除第一个商品能正常输出外，后面的商品输出的“名称”前都有一个“，”不知该怎么去除。
   console.log(receipt);
-
 }
