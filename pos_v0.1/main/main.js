13cd5b0d01eee811caed91f1ee998b62f127abe9
sissie <sissiescofield@outlook.com>
2015-07-23 13:54:43 +0800

    Only pos_v0 finished

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -1,4 +1,83 @@
-//TODO: Please write code in this file.
+var inputs = [
+    {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00
+
+    },
+    {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000001',
+      name: '雪碧',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000001',
+      name: '雪碧',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000004',
+      name: '电池',
+      unit: '个',
+      price: 2.00
+    }
+  ];
+
 function printReceipt(inputs) {
 
+}
+
+function count_same_elements(collection) {
+  var result = [];
+  var temp = objectify(collection);
+  temp.forEach(function(item) {
+    var exist_item = result.filter(function(val){
+      return(item.key === val.key);
+    });
+
+    if(exist_item.length === 0) {
+      result.push(item);
+    } else {
+      exist_item[0].count += item.count;
+    }
+  }) ;
+  return result;
+}
+
+function objectify(collection) {
+  var temp = collection.map(function(val){
+    return {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00
+    } ;
+  });
 }
--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -23,17 +23,21 @@ var inputs = [
   ];
 
 function printReceipt(inputs) {
-  var content = [];
+  var total_price = 0;
+
   inputs.forEach(function(val){
-    content.push('名称：' + val.name + '，数量：' + val.count + val.unit + '，单价：' + val.price.toFixed(2)  + '(元)，小计：' + (val.count*val.price).toFixed(2)  + '(元)' + '\n');
+    total_price += val.price*val.count;
   });
 
-  var sum = 0;
+  var content = '';
+  content += '***<没钱赚商店>收据***' + '\n';
+
   inputs.forEach(function(val){
-    sum += val.price*val.count;
+    content += ('名称：' + val.name + '，数量：' + val.count + val.unit + '，单价：' +
+    val.price.toFixed(2)  + '(元)，小计：' + (val.count*val.price).toFixed(2)  + '(元)' + '\n');
   });
 
-  var receipt = '***<没钱赚商店>收据***' + '\n' + content + '----------------------' + '\n' + '总计：' + sum.toFixed(2) + '(元)' + '\n' + '**********************';
-//由于content是由push()生成的，除第一个商品能正常输出外，后面的商品输出的“名称”前都有一个“，”不知该怎么去除。
-  console.log(receipt);
+  content += ('----------------------' + '\n' + '总计：' + total_price.toFixed(2) + '(元)' + '\n' + '**********************');
+
+  console.log(content);
 }
